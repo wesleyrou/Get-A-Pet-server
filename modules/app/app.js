5f822c85fe1a17994923fe2d207259b16ecc7da9
Wesley Rou <wesleyrou@gmail.com>
2020-08-28 08:32:33 -0700

    eslint formatting

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use('/people', require('../people/people.router'))
-app.use('/pets', require('../pets/pets.router'))
+app.use('/people', require('../people/people.router'));
+app.use('/pets', require('../pets/pets.router'));
 
-module.exports = app
+module.exports = app;
--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -1,5 +1,5 @@
-const Queue = require('../queue/Queue')
-const store = require('../../store')
+const Queue = require('../queue/Queue');
+const store = require('../../store');
 
 // Set up initial data.
 // --------------------
@@ -7,19 +7,20 @@ const store = require('../../store')
 const pets = {
   cats: new Queue(),
   dogs: new Queue()
-}
+};
 
-store.cats.forEach(cat => pets.cats.enqueue(cat))
-store.dogs.forEach(dog => pets.dogs.enqueue(dog))
+store.cats.forEach(cat => pets.cats.enqueue(cat));
+store.dogs.forEach(dog => pets.dogs.enqueue(dog));
 
 // --------------------
 
 module.exports = {
   get() {
     // Return the pets next in line to be adopted.
+    
   },
 
   dequeue(type) {
     // Remove a pet from the queue.
   }
-}
+};
