10d48611559f57e1f5ad529dc6f8c8a4b507e932
Wesley Rou <wesleyrou@gmail.com>
2020-08-28 09:23:05 -0700

    wrote pet service

--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -17,10 +17,32 @@ store.dogs.forEach(dog => pets.dogs.enqueue(dog));
 module.exports = {
   get() {
     // Return the pets next in line to be adopted.
-    
+    let cat = null, dog = null;
+    try{
+      cat = pets.cats.show();
+    }catch(e){
+    }
+
+    try{
+      dog = pets.dogs.show();
+    } catch(e){
+    }    
+
+    return {
+      cat,
+      dog
+    };
   },
 
   dequeue(type) {
     // Remove a pet from the queue.
+    switch(type){
+    case 'dog':
+      return pets.dogs.dequeue();
+    case 'cat':
+      return pets.cats.dequeue();
+    default:
+      return;
+    }
   }
 };
