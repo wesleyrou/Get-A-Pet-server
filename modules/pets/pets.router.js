28850872ddeae6e657ffd6c7a7f91b2392e89f75
Wesley Rou <wesleyrou@gmail.com>
2020-08-28 09:47:46 -0700

    delete endpoint working

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -14,12 +14,43 @@ router.get('/', (req, res) => {
 
 router.delete('/', json, (req, res) => {
   // Remove a pet from adoption.
-  const {whichPet,personName} = req.body;
+  const {whichPet} = req.body;
 
-  // People.dequeue();
-  Pets.dequeue(whichPet);
+  const people = People.get();
+  const pets = Pets.get();
 
-  return res.status(204).json();
+  //If there are no people in Queue
+  if(people.length <= 0){
+    return res.status(400).json('No one in queue');
+  }
+
+  //Choose a pet to adopt
+  switch(whichPet){
+
+  //If there are no dogs in Queue
+  case 'dog':
+    if(pets.dog === null){
+      return res.status(400).json('No dogs to adopt');
+    } else {
+      //Remove a person and a dog from Queue
+      People.dequeue();
+      Pets.dequeue('dog');
+      return res.status(204).end();
+    }
+
+  //If there are no cats in Queue
+  case 'cat':
+    if(pets.cat === null){
+      return res.status(400).json('No cats to adopt');
+    } else {
+      //Remove a person and a cat from Queue
+      People.dequeue();
+      Pets.dequeue('cat');
+      return res.status(204).end();
+    }
+  default:
+    return;
+  }
 });
 
 module.exports = router;
