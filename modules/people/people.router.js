cc0da25735dad13139e1173b7f0a11c1c07c0748
Wesley Rou <wesleyrou@gmail.com>
2020-08-28 08:31:55 -0700

    wrote people get and post endpoints

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const json = require('body-parser').json()
+const express = require('express');
+const json = require('body-parser').json();
 
-const People = require('./people.service')
+const People = require('./people.service');
 
-const router = express.Router()
+const router = express.Router();
 
 router.get('/', (req, res) => {
   // Return all the people currently in the queue.
-})
+  const people = People.get();
+  res.status(200).json(people);
+});
 
 router.post('/', json, (req, res) => {
   // Add a new person to the queue.
-})
+  const {name} = req.body;
+  People.enqueue(name);
 
-module.exports = router
+  res.status(201).json(name);
+});
+
+module.exports = router;
